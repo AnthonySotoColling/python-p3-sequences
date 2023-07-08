6efc77734e35072ab74283a4b2adfffa16c4b69a
anthonyLuisSotoColling <govips@gmail.com>
2023-07-07 18:06:56 -0700

    fib

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    list = [0,1]
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print(list)
+        return
+    for n in range(2,length):
+        list.append(list[n-1]+list[n-2])
+    print(list)
